b5354e9604872df12dcbd15221e0e079108c897e
anca0305 <anca.andronache99@yahoo.com>
2020-01-04 21:46:08 +0200

    Angels

---
+++ angels/AngelsFactory.java
@@ -0,0 +1,33 @@
+package angels;
+
+import common.Constants;
+import angels.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public final class AngelsFactory {
+
+    private AngelsFactory() {
+    }
+
+    public static Angel createAngel(final String angelType, final int round, final int x, final int y) {
+        switch (angelType) {
+            case 'K':
+
+            default:
+                return null;
+        }
+    }
+
+    public static ArrayList<Angel> createAngels(final ArrayList<String> angels,
+                                               final Integer[][] angelsPosition) {
+        ArrayList<Angel> angelsList = new ArrayList<Angel>();
+        int i;
+        for (i = 0; i < angels.size(); i++) {
+            angelsList.add(createAngel(angels.get(i), angelsPosition[i][0], angelsPosition[i][1],
+                    angelsPosition[i][2]);
+        }
+        return angelsList;
+    }
+}
